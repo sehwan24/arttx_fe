354d51b2397c20fb03cbf0560cb4bd2e76f1d6dc
sehwan24 <1224ssanta@naver.com>
2024-09-25 05:22:07 +0900

    [fix] 파일경로수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>캐치! 그림핑 ㅋㅋㅋㅋㅋㅋㅋ수정</h1>
+        <h1>캐치! 그림핑 ㅋㅋㅋㅋㅋㅋㅋ</h1>
         <img src={drawing_ping} alt="drawing_ping" />
       </header>
     </div>
