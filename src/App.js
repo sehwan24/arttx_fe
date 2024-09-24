8aaaf1183624da6bfc18f7ffa39d9bb5f7391bae
sehwan24 <1224ssanta@naver.com>
2024-09-25 04:57:37 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>캐치! 그림핑 ㅋㅋㅋㅋㅋㅋㅋ</h1>
+        <img src="/images/drawing_ping.png" alt="drawing_pingn" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
