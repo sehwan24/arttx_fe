5aec1fe91b2cfb4a0a2741fe95103d403090ef0a
sehwan24 <1224ssanta@naver.com>
2024-12-06 13:59:37 +0900

    [feat] chatting details

--- src/ChattingPage.js
+++ src/ChattingPage.js
@@ -70,11 +70,7 @@ const ChattingPage = () => {
                 }
             );
             const botReply = response.data.data.message || "죄송합니다. 이해하지 못했습니다.";
-            const formattedMessages = Array.isArray(botReply)
-                ? botReply
-                : [{ sender: "bot", text: botReply }];
-            setMessages(formattedMessages);
-            //setMessages((prevMessages) => [...prevMessages, { sender: "bot", text: botReply }]);
+            setMessages((prevMessages) => [...prevMessages, { sender: "bot", text: botReply }]);
         } catch (error) {
             console.error("Error sending message:", error);
             setMessages((prevMessages) => [
